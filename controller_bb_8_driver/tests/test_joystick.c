1405e6558d74d39f58cc11766e4ab8f7c8a9162c
Elena <seryhelena@gmail.com>
2020-09-09 18:08:58 +0300

    fix testJoystick

--- controller_bb_8_driver/tests/test_joystick.c
+++ controller_bb_8_driver/tests/test_joystick.c
@@ -20,17 +20,22 @@ void testJoystick( void )
     int16_t serial_v_y  = 0;
     int16_t serial_w    = 0;
 
+    int16_t buf[3] = {0, 0, 0};
+
     systime_t   time = chVTGetSystemTimeX( );
     while( true )
     {
-        sdRead( &SD6, (uint8_t*) &serial_v_x, 2 );
-        sdRead( &SD6, (uint8_t*) &serial_v_y, 2 );
-        sdRead( &SD6, (uint8_t*) &serial_w, 2 );
+        sdReadTimeout( &SD6, (uint8_t*) &buf, 6, TIME_IMMEDIATE );
+
+        serial_v_x = buf[0];
+        serial_v_y = buf[1];
+        serial_w = buf[2];
+
 
         dbgprintf("x: %d\ty: %d\tw: %d\n\r",
                   serial_v_x, serial_v_y, serial_w);
 
-        time = chThdSleepUntilWindowed( time, time + MS2ST( 50 ) );
+        time = chThdSleepUntilWindowed( time, time + MS2ST( 100 ) );
     }
 
 }
