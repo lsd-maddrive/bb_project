d0348b7317fa9278081ea626f4833d2fd17c4317
Elena <seryhelena@gmail.com>
2020-09-09 15:27:46 +0300

    small fixes in joystick.py

--- python/joystick/joystick.py
+++ python/joystick/joystick.py
@@ -1,69 +1,81 @@
+import logging 
+
 import xbox
 import time
 import math
 import serial
 import struct
 
-port = serial.Serial('/dev/ttyUSB0', 115200)
-port.open()
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 
-# Instantiate the controller
-joy = xbox.Joystick()
+def main():
+    try:
+        port = serial.Serial('/dev/ttyUSB0', 115200)
+        port.open()
+    except FileNotFoundError as err:
+        logger.error(f"USB port is not correct. Connection failed!")
+        return 
 
-V_max = 0.5         # Max linear velocity [m/s]
-ang_speed_max = 1   # Max angular speed [grad/s]
 
-while joy.leftTrigger() < 0.8:
-    # Read joystick data
-    x_axis = joy.leftX()
-    y_axis = joy.leftY()
-    velocity = joy.rightTrigger()
-    ang_speed_raw = joy.rightX()
+    # Instantiate the controller
+    joy = xbox.Joystick()
 
-    # Dead zone for sticks
-    if abs(ang_speed_raw) < 0.15:
-        ang_speed_raw = 0
-    if math.sqrt(x_axis ** 2 + y_axis ** 2) < 0.15:
-        x_axis = 0
-        y_axis = 0
+    V_max = 0.5         # Max linear velocity [m/s]
+    ang_speed_max = 1   # Max angular speed [grad/s]
 
-    # Calculate direction
-    if (x_axis == 0) and (y_axis == 0):
-        angle = -1
-    elif x_axis > 0:
-        angle = math.atan(y_axis / x_axis)
-        if y_axis < 0:
-            angle = angle + math.pi * 2
-    elif x_axis < 0:
-        angle = math.atan(y_axis / x_axis) + math.pi
-    elif x_axis == 0:
-        if y_axis > 0:
-            angle = math.pi / 2
+    while joy.leftTrigger() < 0.8:
+        # Read joystick data
+        x_axis = joy.leftX()
+        y_axis = joy.leftY()
+        velocity = joy.rightTrigger()
+        ang_speed_raw = joy.rightX()
+
+        # Dead zone for sticks
+        if abs(ang_speed_raw) < 0.15:
+            ang_speed_raw = 0
+        if math.sqrt(x_axis ** 2 + y_axis ** 2) < 0.15:
+            x_axis, y_axis = 0, 0
+
+        # Calculate direction
+        if (x_axis == 0) and (y_axis == 0):
+            angle = -1
+        elif x_axis > 0:
+            angle = math.atan(y_axis / x_axis)
+            if y_axis < 0:
+                angle = angle + math.pi * 2
+        elif x_axis < 0:
+            angle = math.atan(y_axis / x_axis) + math.pi
+        elif x_axis == 0:
+            if y_axis > 0:
+                angle = math.pi / 2
+            else:
+                angle = 3 * math.pi / 2
+
+        # To prevent uncertainty
+        if angle == -1:
+            velocity_x, velocity_y = 0, 0
+        # Calculate axial velocity
         else:
-            angle = 3 * math.pi / 2
+            velocity_x = velocity * math.cos(angle) * V_max
+            velocity_y = velocity * math.sin(angle) * V_max
 
-    # To prevent uncertainty
-    if angle == -1:
-        velocity_x = 0
-        velocity_y = 0
-    # Calculate axial velocity
-    else:
-        velocity_x = velocity * math.cos(angle) * V_max
-        velocity_y = velocity * math.sin(angle) * V_max
+        # Just in case
+        if abs(velocity_y) < 0.01:
+            velocity_y = 0
+        if abs(velocity_x) < 0.01:
+            velocity_x = 0
 
-    # Just in case
-    if abs(velocity_y) < 0.01:
-        velocity_y = 0
-    if abs(velocity_x) < 0.01:
-        velocity_x = 0
+        # Calculate angular speed set
+        ang_speed = ang_speed_raw * ang_speed_max
+        data_pack = struct.pack('<hhh', int(velocity_x * 1000), int(velocity_y * 1000), int(ang_speed * 1000))
+        # Here should be data transmit
+        port.write(data_pack)
+        time.sleep(0.1)
+    # Close out when done
+    joy.close()
+    port.close() 
 
-    # Calculate angular speed set
-    ang_speed = ang_speed_raw * ang_speed_max
-    data_pack = struct.pack('<hhh', int(velocity_x * 1000), int(velocity_y * 1000), int(ang_speed * 1000))
-    # Here should be data transmit
-    port.write(data_pack)
-    time.sleep(0.1)
-# Close out when done
-joy.close()
-port.close()
+if __name__ == "__main__":
+    main() 
