960353a7f67b52ed95fa3a845acbc11680ac3894
Elena <seryhelena@gmail.com>
2020-09-10 14:55:43 +0300

    [test_joystick] fix int -> float

--- controller_bb_8_driver/tests/test_joystick.c
+++ controller_bb_8_driver/tests/test_joystick.c
@@ -16,26 +16,16 @@ void testJoystick( void )
     palSetPadMode( GPIOG, 14, PAL_MODE_ALTERNATE(8) );   // TX
     palSetPadMode( GPIOG, 9,  PAL_MODE_ALTERNATE(8) );   // RX
 
-    int16_t serial_v_x  = 0;
-    int16_t serial_v_y  = 0;
-    int16_t serial_w    = 0;
-
-    int16_t buf[3] = {0, 0, 0};
+    float buf[3] = {0, 0, 0};
 
     systime_t   time = chVTGetSystemTimeX( );
     while( true )
     {
         sdReadTimeout( &SD6, (uint8_t*) &buf, 6, TIME_IMMEDIATE );
 
-        serial_v_x = buf[0];
-        serial_v_y = buf[1];
-        serial_w = buf[2];
-
-
         dbgprintf("x: %d\ty: %d\tw: %d\n\r",
-                  serial_v_x, serial_v_y, serial_w);
+                  buf[0] * 100, buf[1] * 100, buf[2] * 100);
 
         time = chThdSleepUntilWindowed( time, time + MS2ST( 100 ) );
     }
-
 }
