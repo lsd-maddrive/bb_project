8988e781398d91df16ebd3c5c929f2db248b4516
Elena <seryhelena@gmail.com>
2020-09-22 14:26:28 +0300

    xbox_one - type fixes

--- python/joystick/src/xbox_one.py
+++ python/joystick/src/xbox_one.py
@@ -11,17 +11,17 @@ class Joystick:
     def close(self):
         self.device.close()
         
-    def leftXAxis(self)
+    def leftXAxis(self):
         return -(self.device.absinfo(1).value / (self.device.absinfo(1).max + 1))
         
-    def leftYAxis(self)
+    def leftYAxis(self):
         return self.device.absinfo(0).value / (self.device.absinfo(0).max + 1)
         
-    def rightYAxis(self)
+    def rightYAxis(self):
         return self.device.absinfo(3).value / (self.device.absinfo(3).max + 1)
         
-    def leftTrigger(self)
+    def leftTrigger(self):
         return self.device.absinfo(2).value / (self.device.absinfo(2).max + 1)
         
-    def rightTrigger(self)
+    def rightTrigger(self):
         return self.device.absinfo(5).value / (self.device.absinfo(5).max + 1)
