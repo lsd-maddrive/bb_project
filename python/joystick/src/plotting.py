cf792da4394c642b10516b21dedb6627e565ea87
Гриша <grigory@megahrun.squirrelhome>
2020-10-31 02:15:23 +0300

    Logger adjustment

--- python/joystick/src/plotting.py
+++ python/joystick/src/plotting.py
@@ -5,12 +5,13 @@ from os import listdir
 pyplot.style.use('ggplot')  # Красивые графики
 pyplot.rcParams['figure.figsize'] = (15, 5)  # Размер картинок
 
-file_to_plot = ''
+file_to_plot = 'biostats.csv'
 
 csv_file = '../Logs/' + file_to_plot
 if csv_file == '../Logs/':
     all_files = [f for f in listdir(csv_file) if f.endswith('.csv')]
     csv_file = ''.join([csv_file, all_files[-1]])
 
-log = pandas.read_csv(csv_file, sep=',', encoding='latin1', index_col='time')
-log['par1'].plot(figsize=(15, 10))
+log = pandas.read_csv(csv_file, sep=',')
+log['time'].plot(figsize=(15, 10))
+#log['val1'].plot(figsize=(15, 10))
